494b0b3700118d031ae76f1e0e0d1b4b06e7de0f
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2025-11-21 05:51:43 -0300

    feat: improve currency validation

--- internal/validators/validators.go
+++ internal/validators/validators.go
@@ -9,6 +9,17 @@ import (
 )
 
 func ValidateCurrency(baseCurrency, convertCurrency string) {
+	if baseCurrency == "" {
+		pterm.Error.Println("missing base currency")
+		os.Exit(1)
+		return
+	}
+	if convertCurrency == "" {
+		pterm.Error.Println("missing convert currency")
+		os.Exit(1)
+		return
+	}
+
 	if baseCurrency == convertCurrency {
 		pterm.Error.Printf("cannot convert money into the same currency (%s)\n", baseCurrency)
 		os.Exit(1)
